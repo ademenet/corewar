47a86f5a323965658e8dd9d53daf754ed90adced
Visentin <tvisenti@42.student.fr>
2016-07-11 19:43:19 +0200

    Start parsing

--- asm/src/asm_main.c
+++ asm/src/asm_main.c
@@ -6,6 +6,22 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 15:50:23 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/11 15:50:34 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/07/11 19:31:28 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../inc/asm.h"
+
+int		main(int argc, char **argv)
+{
+	t_header	*head;
+	char		*line;
+
+	line = NULL;
+	head = asm_header_init();
+	if (argc == 2)
+		asm_parsing(argv[1], head);
+	else
+		ft_printf("Put one champion, please !")
+	return (0);
+}
