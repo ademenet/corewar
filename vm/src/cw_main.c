feaf68650010a666441b25182accb698799ce152
Alain DEMENET <ademenet@student.42.fr>
2016-07-11 20:11:34 +0200

    Start parameters handling

--- vm/src/cw_main.c
+++ vm/src/cw_main.c
@@ -6,6 +6,21 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 15:54:35 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/11 15:54:35 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/07/11 19:38:04 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../inc/corewar.h"
+
+int				main(int argc, char **argv)
+{
+	char	mem[MEM_SIZE];
+
+	if (argc < 3)
+		cw_display_helps();
+	else
+		cw_parsing();
+	return (0);
+}
+
+// dans cw_parsing() y'aura cw_params_handler()
