3525d292d4be2a38a8c233c5cb1eedfa5972df80
Visentin <tvisenti@42.student.fr>
2016-07-11 19:40:56 +0200

    asm.h : Add functions

--- asm/inc/asm.h
+++ asm/inc/asm.h
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 18:10:01 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/07/11 18:13:30 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/07/11 19:36:37 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,25 @@
 ** Header
 */
 
-typedef struct		header_s
+typedef struct		s_header
 {
   unsigned int		magic;
   char				prog_name[PROG_NAME_LENGTH + 1];
   unsigned int		prog_size;
   char				comment[COMMENT_LENGTH + 1];
-}					header_t;
+}					t_header;
+
+/*
+** Library
+*/
+
+t_header	*asm_header_init(void);
+
+/*
+** Source
+*/
+
+int			asm_name_comment(char *line, t_header *head);
+int			asm_parsing(char *line, t_header *head);
 
 #endif
