9f7e75f068a116db5e41c46a9a6cb876df568785
Visentin <tvisenti@42.student.fr>
2016-07-18 17:14:20 +0200

    Add function join + free

--- asm/lib/asm_free.c
+++ asm/lib/asm_free.c
@@ -6,7 +6,7 @@
 /*   By: Transmet <Transmet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/16 16:13:52 by Transmet          #+#    #+#             */
-/*   Updated: 2016/07/16 16:31:03 by Transmetropolitan###   ########.fr       */
+/*   Updated: 2016/07/18 16:49:44 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,26 @@ int		asm_free_tab(char **tab, int ret)
 		free(tab);
 	}
 	return (ret);
+}
+
+/*
+**
+*/
+
+void	asm_free_join(char *line)
+{
+	if (*line)
+	{
+		if (!g_file)
+		{
+			g_file = ft_strdup(line);
+			g_file = ft_strjoin(g_file, "\n");
+		}
+		else
+		{
+			g_file = realloc(g_file, ft_strlen(line) + ft_strlen(g_file) + 1);
+			g_file = ft_strjoin(g_file, line);
+			g_file = ft_strjoin(g_file, "\n");
+		}
+	}
 }
