716008750ae329bb7641fb3e4436dee57e83b5a8
Visentin <tvisenti@42.student.fr>
2016-07-18 17:15:54 +0200

    Add check_valid_line, comment

--- asm/src/asm_parsing.c
+++ asm/src/asm_parsing.c
@@ -6,43 +6,14 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 18:53:48 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/07/18 11:53:16 by gseropia         ###   ########.fr       */
+/*   Updated: 2016/07/18 17:05:27 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
 
 /*
-** Verifie les instructions
-*/
-
-int		asm_check_instruct(char *line)
-{
-	char	**tab;
-	int		ret;
-
-	ret = 0;
-	
-	if (ft_strchr(line, '\t'))
-		tab = ft_strsplit(line, '\t');
-	else
-		tab = ft_strsplit(line, ' ');
-
-	if (tab[0])
-	{
-		//ft_printf("test1\n");
-		ret = asm_instruct_name(&line);
-		//printf("ARG : %d\n", ret);
-		// printf("ARG : %d\n", ret);
-		// asm_check_arg(line, ret);
-	}
-	//ft_printf("test2\n");
-	return (asm_free_tab(tab, 1));
-}
-
-/*
 ** Récupére le nom et le comment et le stocke dans la struct(header)
-** !!!! FAIRE DES FREE + RETOUR ERROR !!!!
 */
 
 int		asm_copy_name_comment(char *line, t_header *head, int first, int last)
@@ -84,14 +55,21 @@ int		asm_handler_name_comment(int fd, char *line, t_header *head)
 			last = ft_strlen(COMMENT_CMD_STRING);
 			asm_copy_name_comment(line, head, first, last);
 			if (head->comment[0] && head->prog_name[0])
-				return(1);
+				return (1);
 		}
 	}
 	return (0);
 }
-int check_valid_line(char *line)
+
+/*
+** Incrémente line selon l'instruction passé
+*/
+
+int		check_valid_line(char *line)
 {
-	int fct = asm_instruct_name(&line);
+	int fct;
+
+	fct = asm_instruct_name(&line);
 	g_pos++;
 	if (fct == 1 || fct == 9 || fct == 12 || fct == 14)
 		line = line + 4;
@@ -101,16 +79,14 @@ int check_valid_line(char *line)
 		line = line + 5;
 	else
 		line = line + 3;
-	//ft_printf("test line :%s\n", line); 
 	if (!asm_check_arg(line, fct))
 		return(asm_error(8));
 	line = NULL;
-return(0);	
+	return (1);
 }
 
 /*
-** Join + free; voir realloc ?
-** !!!! Il faut traiter le label !!!!
+** Récursive qui récupere le nom du label, position dans une liste chainée
 */
 
 t_label		*asm_parse_line(char *line, int fd, int check)
@@ -120,54 +96,56 @@ t_label		*asm_parse_line(char *line, int fd, int check)
 
 	ret = 1;
 	new = NULL;
-
-	if (check == 1 && g_line++)
-		ret = get_next_line(fd, &line);
-	ft_printf("text: %s ---- g_line : %d ----g_pos : %d\n", line, g_line, g_pos);
+	if (check == 1 && (ret = get_next_line(fd, &line)) && g_line++)
+		asm_free_join(line);
 	if (ret > 0 && line[0] != COMMENT_CHAR && asm_check_label(line) >= 1)
 	{
 		new = asm_label_init();
 		new->name = ft_strsub(line, 0, ft_strclen(line, LABEL_CHAR));
 		new->pos = g_pos;
-		// Enregistrer la suite du label //
 		if (asm_check_label(line) == 1)
 			new->next = asm_parse_line(line, fd, 1);
 		else
-			new->next = asm_parse_line(line + ft_strclen(line, LABEL_CHAR) + 1, fd, 0);
+			new->next = asm_parse_line(line +
+				ft_strclen(line, LABEL_CHAR) + 1, fd, 0);
 	}
-	else if (ret && line[0] != COMMENT_CHAR && line[0] != '\0' && asm_check_label(line) == 0)
-	{
-		// printf("Found instruct :%s\n", line);
-		check_valid_line(line);
-		return(asm_parse_line(line, fd, 1));
-	}
+	else if (ret && line[0] != COMMENT_CHAR && line[0] != '\0' &&
+	asm_check_label(line) == 0 && check_valid_line(line))
+		return (asm_parse_line(line, fd, 1));
 	else if (ret > 0)
-		return(asm_parse_line(line, fd, 1));
+		return (asm_parse_line(line, fd, 1));
 	return (new);
 }
 
+/*
+** Début du parsing
+*/
+
 int			asm_parsing(char *champion, t_header *head)
 {
-    int			fd;
-    int			pos;
-    char		*line;
-    t_label		*label;
+	int			fd;
+	int			pos;
+	char		*line;
+	t_label		*label;
 
-    pos = 0;
-    line = NULL;
-    if ((fd = open(champion, O_RDONLY, 0555)) == -1)
-        return (-1);
-    if (asm_handler_name_comment(fd, line, head) == 0)
-        return (0);
+	pos = 0;
+	line = NULL;
+	if ((fd = open(champion, O_RDONLY, 0555)) == -1)
+		return (-1);
+	if (asm_handler_name_comment(fd, line, head) == 0)
+		return (0);
 	label = asm_parse_line(line, fd, 1);
+	asm_check_double_label(label);
 	// Verification de l'enregistrement des labels
-	// while(label)
-    // {
-    //     printf("Label : -%s-\n", label->name);
-    //     label = label->next;
-    // }
+	// while (label)
+	// {
+	// 	printf("Label : -%s-\t", label->name);
+	// 	printf("Position : |%d|\n", label->pos);
+	// 	label = label->next;
+	// }
+	// printf("G_file : \n%sEND", g_file);
 	// Verification de l'enregistrement du nom et comment
 	// printf("Name : |%s|\n", head->prog_name);
 	// printf("Comment : |%s|\n", head->comment);
-    return (0);
+	return (0);
 }
