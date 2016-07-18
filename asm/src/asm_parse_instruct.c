4bf6f98bdd20e03f93e99b5d85299980cb7c87da
Visentin <tvisenti@42.student.fr>
2016-07-18 17:16:38 +0200

    Add check_double_label

--- asm/src/asm_parse_instruct.c
+++ asm/src/asm_parse_instruct.c
@@ -6,24 +6,23 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 13:05:23 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/07/17 15:10:04 by Transmetropolitan###   ########.fr       */
+/*   Updated: 2016/07/18 17:13:13 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
 
 /*
-** Verifie si le nom de l'instruction est correct
+** Verifie si le nom de l'instruction est correct et retourne
+** l'int correspondant
 */
 
 int			asm_instruct_name_sec(char **line)
 {
 	if (ft_strncmp("ldi", *line, 3) == 0)
 		return (10);
-	//printf("On pointe ici : --%c--\n", *line);
 	if (ft_strncmp("ld", *line, 2) == 0)
 		return (2);
-	//printf("On pointe la : --%c--\n", *line);
 	if (ft_strncmp("fork", *line, 4) == 0)
 		return (12);
 	if (ft_strncmp("st", *line, 2) == 0)
@@ -42,11 +41,7 @@ int			asm_instruct_name_sec(char **line)
 int			asm_instruct_name(char **line)
 {
 	while (**line == ' ' || **line == '\t')
-	{
-		//printf("On pointe ici : --%c--\n",*line);
 		(*line)++;
-	}
-	//printf("On pointe la : --%c--\n",*line);
 	if (ft_strncmp("live", *line, 4) == 0)
 		return (1);
 	if (ft_strncmp("add", *line, 3) == 0)
@@ -96,4 +91,46 @@ int			asm_check_label(char *str)
 		return(asm_error(3));
 	}
 	return (0);
+}
+
+/*
+** Check si il y a un doublon dans les labels
+*/
+
+int			asm_check_double_label(t_label *label)
+{
+	t_label *start;
+
+	start = label;
+	if (!label)
+		return (1);
+	label = label->next;
+	while (label)
+	{
+		if (!ft_strcmp(start->name, label->name))
+			return (asm_error(9));
+		label = label->next;
+	}
+	return(asm_check_double_label(start->next));
+}
+
+/*
+** Verifie les instructions
+*/
+
+int		asm_check_instruct(char *line)
+{
+	char	**tab;
+	int		ret;
+
+	ret = 0;
+
+	if (ft_strchr(line, '\t'))
+		tab = ft_strsplit(line, '\t');
+	else
+		tab = ft_strsplit(line, ' ');
+
+	if (tab[0])
+		ret = asm_instruct_name(&line);
+	return (asm_free_tab(tab, 1));
 }
