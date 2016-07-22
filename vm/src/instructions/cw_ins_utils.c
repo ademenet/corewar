4012e9f161449334064f17abce6e40985bb3fa58
Alain DEMENET <ademenet@student.42.fr>
2016-07-22 16:52:37 +0200

    Initie le travail sur la reconnaissance des types de params

---
+++ vm/src/instructions/cw_ins_utils.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cw_ins_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/22 15:19:42 by ademenet          #+#    #+#             */
+/*   Updated: 2016/07/22 15:28:33 by ademenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/corewar.h"
+
+/*
+**
+*/
+
+// TODO : trouver un moyen rapide et simple (pour toutes les fonctions
+// d'instructions) de recuperer les types de params.
