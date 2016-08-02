d5e19d2d33a42570f389786d7e941138933a1f60
Visentin <tvisenti@42.student.fr>
2016-08-02 19:24:31 +0200

    Xor, or, and -> separer en fonction

--- vm/src/cw_ins_and.c
+++ vm/src/cw_ins_and.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:14:06 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/26 15:17:35 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/02 19:19:49 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,25 @@
 ** modifie le carry.
 */
 
+void		cw_exec_and(t_champion *tmp, unsigned int total, unsigned int p[3])
+{
+	tmp->reg[p[2] - 1][0] = total >> 24;
+	tmp->reg[p[2] - 1][1] = total >> 16;
+	tmp->reg[p[2] - 1][2] = total >> 8;
+	tmp->reg[p[2] - 1][3] = total;
+}
+
 int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
 
 	unsigned int	p_sze[3];
 	unsigned int	p[3];
 	unsigned int	total;
+
 	total = 0;
 	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
-
 	if (ocp->first == REG_CODE)
 		p[0] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2] - 1);
 	else if (ocp->first == DIR_CODE)
@@ -40,23 +48,16 @@ int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
 		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
-
 	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
-
-	mvprintw(6, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
-
 	total = p[1] & p[0];
-
-	mvprintw(30, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
-
-	ft_printf("reg avant stock : %s\n", tmp->reg[p[2] - 1]);
-
-	tmp->reg[p[2] - 1][0] = total >> 24;
-	tmp->reg[p[2] - 1][1] = total >> 16;
-	tmp->reg[p[2] - 1][2] = total >> 8;
-	tmp->reg[p[2] - 1][3] = total;
-//	mvprintw(5, 210,"reg :%c, %c, %c, %c avant stockage\n", tmp->reg[p[2] - 1][0],tmp->reg[p[2] - 1][1],tmp->reg[p[2] - 1][2],tmp->reg[p[2] - 1][3]);
-	//ft_printf("reg apres stock : %s\n", tmp->reg[p[2] - 1]);
-	mvprintw(20, 210,"reg :%s apres stockage\n", tmp->reg[p[2] - 1]);
+	cw_exec_and(tmp, total, p);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
+
+
+// mvprintw(6, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
+// mvprintw(30, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
+// ft_printf("reg avant stock : %s\n", tmp->reg[p[2] - 1]);
+// mvprintw(5, 210,"reg :%c, %c, %c, %c avant stockage\n", tmp->reg[p[2] - 1][0],tmp->reg[p[2] - 1][1],tmp->reg[p[2] - 1][2],tmp->reg[p[2] - 1][3]);
+// ft_printf("reg apres stock : %s\n", tmp->reg[p[2] - 1]);
+// mvprintw(20, 210,"reg :%s apres stockage\n", tmp->reg[p[2] - 1]);
--- vm/src/cw_ins_or.c
+++ vm/src/cw_ins_or.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 12:57:01 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/26 15:17:42 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/02 19:22:41 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,25 @@
 ** Même que and mais avec le ou ( | du c).
 */
 
+static void	cw_exec_or(t_champion *tmp, unsigned int total, unsigned int p[3])
+{
+	tmp->reg[p[2] - 1][0] = total >> 24;
+	tmp->reg[p[2] - 1][1] = total >> 16;
+	tmp->reg[p[2] - 1][2] = total >> 8;
+	tmp->reg[p[2] - 1][3] = total;
+}
+
 int			cw_ins_or(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
+
 	unsigned int	p_sze[3];
 	unsigned int	p[3];
 	unsigned int	total;
+
 	total = 0;
 	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
-
 	if (ocp->first == REG_CODE)
 		p[0] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2] - 1);
 	else if (ocp->first == DIR_CODE)
@@ -38,12 +47,8 @@ int			cw_ins_or(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
 		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
-
 	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
 	total = p[1] | p[0];
-	tmp->reg[p[2] - 1][0] = total >> 24;
-	tmp->reg[p[2] - 1][1] = total >> 16;
-	tmp->reg[p[2] - 1][2] = total >> 8;
-	tmp->reg[p[2] - 1][3] = total;
+	cw_exec_and(tmp, total, p);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
