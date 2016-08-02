1d1f98bbbde7e1b6cf6a11275120afeea240862d
Geoffrey SEROPIAN <gseropia@e3r5p10.42.fr>
2016-08-02 18:05:41 +0200

    fonctions ok pas encore normees

--- vm/src/cw_ins_and.c
+++ vm/src/cw_ins_and.c
@@ -23,6 +23,7 @@ int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	unsigned int	p_sze[3];
 	unsigned int	p[3];
 	unsigned int	total;
+	total = 0;
 	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
@@ -32,24 +33,29 @@ int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	else if (ocp->first == DIR_CODE)
 		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4);
 	else if (ocp->first == IND_CODE)
-		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2);
+		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2) % IDX_MOD;
 	if (ocp->second == REG_CODE)
 		p[1] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2 + p_sze[0]] - 1);
 	else if (ocp->second == DIR_CODE)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
-		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]);
+		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
+
 	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
+
 	mvprintw(6, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
+
 	total = p[1] & p[0];
-	mvprintw(10, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
+
+	mvprintw(30, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
+
 	ft_printf("reg avant stock : %s\n", tmp->reg[p[2] - 1]);
-//
+
 	tmp->reg[p[2] - 1][0] = total >> 24;
-	tmp->reg[p[2] - 1][1] = (total << 8) >> 24;
-	tmp->reg[p[2] - 1][2] = (total << 16) >> 24;
-	tmp->reg[p[2] - 1][3] = (total << 24) >> 24;
-	mvprintw(5, 210,"reg :%c, %c, %c, %c avant stockage\n", tmp->reg[p[2] - 1][0],tmp->reg[p[2] - 1][1],tmp->reg[p[2] - 1][2],tmp->reg[p[2] - 1][3]);
+	tmp->reg[p[2] - 1][1] = total >> 16;
+	tmp->reg[p[2] - 1][2] = total >> 8;
+	tmp->reg[p[2] - 1][3] = total;
+//	mvprintw(5, 210,"reg :%c, %c, %c, %c avant stockage\n", tmp->reg[p[2] - 1][0],tmp->reg[p[2] - 1][1],tmp->reg[p[2] - 1][2],tmp->reg[p[2] - 1][3]);
 	//ft_printf("reg apres stock : %s\n", tmp->reg[p[2] - 1]);
 	mvprintw(20, 210,"reg :%s apres stockage\n", tmp->reg[p[2] - 1]);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
--- vm/src/cw_ins_xor.c
+++ vm/src/cw_ins_xor.c
@@ -18,5 +18,32 @@
 
 int			cw_ins_xor(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-	return (1);
+	unsigned int	p_sze[3];
+	unsigned int	p[3];
+	unsigned int	total;
+	total = 0;
+	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
+	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
+	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
+
+	if (ocp->first == REG_CODE)
+		p[0] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2] - 1);
+	else if (ocp->first == DIR_CODE)
+		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4);
+	else if (ocp->first == IND_CODE)
+		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2) % IDX_MOD;
+	if (ocp->second == REG_CODE)
+		p[1] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2 + p_sze[0]] - 1);
+	else if (ocp->second == DIR_CODE)
+		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
+	else if (ocp->second == IND_CODE)
+		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
+
+	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
+	total = p[1] ^ p[0];
+	tmp->reg[p[2] - 1][0] = total >> 24;
+	tmp->reg[p[2] - 1][1] = total >> 16;
+	tmp->reg[p[2] - 1][2] = total >> 8;
+	tmp->reg[p[2] - 1][3] = total;
+	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
