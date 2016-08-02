b571b87ec25b5de4b1d3317da6c2f2a1039eeca2
Alain DEMENET <ademenet@student.42.fr>
2016-08-02 19:28:33 +0200

    En cours

--- vm/src/cw_ins_ld.c
+++ vm/src/cw_ins_ld.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:11:59 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/02 18:00:39 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/02 18:50:58 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,33 @@
 
 int			cw_ins_ld(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-	unsigned int	param;
-	unsigned char	reg;
+	// unsigned int	param;
+	// unsigned char	reg;
+	unsigned int	p_sze[2];
+	unsigned int    p[2];
+
+	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
+	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
+	if (ocp->first == DIR_CODE)
+		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4) % IDX_MOD;
+	else if (ocp->first == IND_CODE)
+		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2) % IDX_MOD;
+	p[1] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2 + p_sze[0]] - 1);
+	if (p[1] < 1 || p[1] > REG_NUMBER || ocp->first == REG_CODE)
+		return (1 + 1 + p_sze[0] + p_sze[1]);
 
 
-	if (ocp->first == IND_CODE)
-	{
-		param = cw_get_data_ind(proc, tmp, (tmp->pc + 2) % MEM_SIZE);
-		reg = proc->mem[(tmp->pc + 4) % MEM_SIZE];
-	}
-	else
-	{
-		param = cw_get_data_dir(proc, tmp, (tmp->pc + 2) % MEM_SIZE, 4);
-		reg = proc->mem[(tmp->pc + 6) % MEM_SIZE];
-	}
+
+	// if (ocp->first == IND_CODE)
+	// {
+	// 	param = cw_get_data_ind(proc, tmp, (tmp->pc + 2) % MEM_SIZE);
+	// 	reg = proc->mem[(tmp->pc + 4) % MEM_SIZE];
+	// }
+	// else
+	// {
+	// 	param = cw_get_data_dir(proc, tmp, (tmp->pc + 2) % MEM_SIZE, 4);
+	// 	reg = proc->mem[(tmp->pc + 6) % MEM_SIZE];
+	// }
 
 	mvprintw(0, 0, "%.8x & %.2hhx", param, reg);
 	// Pour load la valeur dans le registre :
