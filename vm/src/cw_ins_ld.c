e9408adacc62bbde0032ca9264aa437ebda2e2e6
Alain DEMENET <ademenet@student.42.fr>
2016-08-02 12:19:22 +0200

    Initialise

--- vm/src/cw_ins_ld.c
+++ vm/src/cw_ins_ld.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:11:59 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/01 13:16:23 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/02 11:37:19 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,20 @@
 ** le registre. Cette opération modifie le carry.
 */
 
+int			cw_ins_ld_size(t_ocp *ocp)
+{
+	return (size);
+}
+
 int			cw_ins_ld(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
 
-	return (7);
+
+	// Pour load la valeur dans le registre :
+	tmp->reg[proc->mem[tmp->pc + 4] - 1][0] = param[2] >> 24;
+	tmp->reg[proc->mem[tmp->pc + 4] - 1][1] = param[2] >> 16;
+	tmp->reg[proc->mem[tmp->pc + 4] - 1][2] = param[2] >> 8;
+	tmp->reg[proc->mem[tmp->pc + 4] - 1][3] = param[2];
+	tmp->carry = tmp->carry == 0 ? 1 : 0;
+	return (cw_ins_ld_size(ocp));
 }
