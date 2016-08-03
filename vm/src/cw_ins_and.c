912d5832a0368211f29f2938fcbd230a3e29a10f
Visentin <tvisenti@42.student.fr>
2016-08-03 11:38:31 +0200

    Fonction ok

--- vm/src/cw_ins_and.c
+++ vm/src/cw_ins_and.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:14:06 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/02 19:19:49 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/08/03 11:36:04 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 ** modifie le carry.
 */
 
-void		cw_exec_and(t_champion *tmp, unsigned int total, unsigned int p[3])
+static void	cw_exec_and(t_champion *tmp, unsigned int total, unsigned int p[3])
 {
 	tmp->reg[p[2] - 1][0] = total >> 24;
 	tmp->reg[p[2] - 1][1] = total >> 16;
@@ -27,7 +27,6 @@ void		cw_exec_and(t_champion *tmp, unsigned int total, unsigned int p[3])
 
 int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-
 	unsigned int	p_sze[3];
 	unsigned int	p[3];
 	unsigned int	total;
@@ -37,23 +36,23 @@ int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
 	if (ocp->first == REG_CODE)
-		p[0] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2] - 1);
+		p[0] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1);
 	else if (ocp->first == DIR_CODE)
 		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4);
 	else if (ocp->first == IND_CODE)
-		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2) % IDX_MOD;
+		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2);
 	if (ocp->second == REG_CODE)
-		p[1] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2 + p_sze[0]] - 1);
+		p[1] = cw_get_data_reg(tmp,
+			proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] - 1);
 	else if (ocp->second == DIR_CODE)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
-		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
-	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
+		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]);
+	p[2] = proc->mem[(tmp->pc + 2 + p_sze[0] + p_sze[1]) % MEM_SIZE];
 	total = p[1] & p[0];
 	cw_exec_and(tmp, total, p);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
-
 
 // mvprintw(6, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
 // mvprintw(30, 210, "p0 : %d, p1 : %d et total :%d\n",p[0], p[1], total);
--- vm/src/cw_ins_or.c
+++ vm/src/cw_ins_or.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/14 12:57:01 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/02 19:22:41 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/08/03 11:35:04 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ static void	cw_exec_or(t_champion *tmp, unsigned int total, unsigned int p[3])
 
 int			cw_ins_or(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-
 	unsigned int	p_sze[3];
 	unsigned int	p[3];
 	unsigned int	total;
@@ -36,19 +35,20 @@ int			cw_ins_or(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
 	if (ocp->first == REG_CODE)
-		p[0] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2] - 1);
+		p[0] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1);
 	else if (ocp->first == DIR_CODE)
 		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4);
 	else if (ocp->first == IND_CODE)
-		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2) % IDX_MOD;
+		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2);
 	if (ocp->second == REG_CODE)
-		p[1] = cw_get_data_reg(tmp, proc->mem[tmp->pc + 2 + p_sze[0]] - 1);
+		p[1] = cw_get_data_reg(tmp,
+			proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] - 1);
 	else if (ocp->second == DIR_CODE)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
-		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]) % IDX_MOD;
-	p[2] = proc->mem[tmp->pc + 2 + p_sze[0] + p_sze[1]];
+		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]);
+	p[2] = proc->mem[(tmp->pc + 2 + p_sze[0] + p_sze[1]) % MEM_SIZE];
 	total = p[1] | p[0];
-	cw_exec_and(tmp, total, p);
+	cw_exec_or(tmp, total, p);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
