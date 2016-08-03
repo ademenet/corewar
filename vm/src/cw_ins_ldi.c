93650d9d1a3941a94f303e065ab6a1c843c57a8c
Alain DEMENET <ademenet@student.42.fr>
2016-08-03 16:44:23 +0200

    En cours de code le ldio

--- vm/src/cw_ins_ldi.c
+++ vm/src/cw_ins_ldi.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:15:25 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/26 15:17:37 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/03 16:32:38 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,27 @@
 
 int			cw_ins_ldi(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
+	// unsigned int	p[2];
+	unsigned int	psze[3];
+
+	psze[0] = cw_ins_param_sze(ocp->first, 2);
+	psze[1] = cw_ins_param_sze(ocp->second, 2);
+	psze[2] = cw_ins_param_sze(ocp->third, 2);
+
+	mvprintw(0, 210, "%u %u %u", psze[0], psze[1], psze[2]);
+	// if (ocp->first == REG_CODE)
+	// 	p[0] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE])
+	// 		% IDX_MOD;
+	// else if (ocp->first == DIR_CODE)
+	// 	p[0] = cw_get_data_dir();
+	// else if (ocp->first == IND_CODE)
+	// 	p[0] = cw_get_data_ind();
+	// if (ocp->second == DIR_CODE)
+	// 	p[1] = cw_get_data_dir()
+	// else if (ocp->second == REG_CODE)
+	// 	p[1] = cw_get_data_reg(tmp, )
+	// if (ocp->third != REG_CODE) // gestion des erreurs
+
+	tmp->carry = tmp->carry == 0 ? 1 : 0;
 	return (1);
 }
