df634f3a4940ca4d0781d224eedb5726ac916be1
Alain DEMENET <ademenet@student.42.fr>
2016-08-06 18:19:41 +0200

    Resultat semble correct, a tester

--- vm/src/cw_ins_ldi.c
+++ vm/src/cw_ins_ldi.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:15:25 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/04 17:11:52 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/08/06 18:18:47 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 ** l’adresse : (PC + (3 % IDX_MOD)) ajoute 4 à cette valeur. On nommera S
 ** cette somme. On lit REG_SIZE octet à l’adresse (PC + (S % IDX_MOD))
 ** que l’on copie dans r1. Les paramètres 1 et 2 sont des index.
+** T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG
 */
 
 int			cw_ins_ldi(t_proc *proc, t_champion *tmp, t_ocp *ocp)
@@ -29,24 +30,34 @@ int			cw_ins_ldi(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	p_sze[1] = cw_ins_param_sze(ocp->second, 2);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 2);
 	mvprintw(0, 210, "%u %u %u", p_sze[0], p_sze[1], p_sze[2]);
+
 	if (ocp->third != REG_CODE)
 		return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
+
 	if (ocp->first == REG_CODE)
 		p[0] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE]) % IDX_MOD;
 	else if (ocp->first == DIR_CODE)
 		p[0] = cw_get_data_dir(proc, tmp, (tmp->pc + 2) % IDX_MOD, 2);
 	else if (ocp->first == IND_CODE)
 		p[0] = cw_get_data_ind(proc, tmp, (tmp->pc + 2) % IDX_MOD);
+
 	if (ocp->second == REG_CODE)
 		p[1] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] % IDX_MOD);
 	else if (ocp->second == DIR_CODE)
 		p[1] = cw_get_data_dir(proc, tmp, (tmp->pc + 2 + p_sze[0]) % IDX_MOD, 2);
+
 	p[2] = proc->mem[(tmp->pc + 2 + p_sze[0] + p_sze[1]) % MEM_SIZE] % IDX_MOD;
-	ret = cw_get_data_ind(proc, tmp, proc->mem[(p[0] + p[1]) % MEM_SIZE]) % IDX_MOD;
+
+	ret = cw_get_data_ind(proc, tmp, (p[0] + p[1]) % MEM_SIZE);
+
 	tmp->reg[p[2] - 1][0] = ret >> 24;
 	tmp->reg[p[2] - 1][1] = ret >> 16;
 	tmp->reg[p[2] - 1][2] = ret >> 8;
 	tmp->reg[p[2] - 1][3] = ret;
+
+	mvprintw(1, 210, "%u || %u || %u || ret = %u", p[0], p[1], p[2], ret);
+	mvprintw(2, 210, "%.2hhx %.2hhx %.2hhx %.2hhx", tmp->reg[p[2] - 1][0], tmp->reg[p[2] - 1][1], tmp->reg[p[2] - 1][2], tmp->reg[p[2] - 1][3]);
+
 	tmp->carry = tmp->carry == 0 ? 1 : 0;
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
 }
