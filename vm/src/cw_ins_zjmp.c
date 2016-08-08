37097cd2c84d12c1350ba440e37d447e2363e780
Alain DEMENET <ademenet@student.42.fr>
2016-08-08 17:13:37 +0200

    zjmp ne prend pas de IDX_MOD (pour pouvoir jumper en negatif...) - a verifier toutefois si les conditions ne sont pas plus compliquees

--- vm/src/cw_ins_zjmp.c
+++ vm/src/cw_ins_zjmp.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:14:38 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/08 15:44:12 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/08 17:12:13 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int			cw_ins_zjmp(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
 	unsigned int	p;
 
-	p = cw_get_data_dir(proc, tmp, (tmp->pc + 1) % MEM_SIZE, 2) % IDX_MOD;
+	p = cw_get_data_dir(proc, tmp, (tmp->pc + 1) % MEM_SIZE, 2);
 	if (tmp->carry == 1 && p > 3)
 		return (p);
 	return (3);
