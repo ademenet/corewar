78453b887d861c8e5d0315859deba3ae2b821e93
Alain DEMENET <ademenet@student.42.fr>
2016-08-11 15:06:43 +0200

    Ajout de st en vu d'ajouter affichage dans vm

--- vm/src/cw_ins_st.c
+++ vm/src/cw_ins_st.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:12:38 by ademenet          #+#    #+#             */
-/*   Updated: 2016/07/26 15:17:43 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/11 15:06:20 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,27 @@
 
 int			cw_ins_st(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-	
-	return (1);
+	unsigned int	p_sze[2];
+	unsigned int	p[2];
+	unsigned int	i;
+
+	i = -1;
+	p_sze[0] = 1;
+	p_sze[1] = cw_ins_param_sze(ocp->second, 2);
+	p[0] = proc->mem[(tmp->pc + 2) % MEM_SIZE];
+	p[0] = cw_get_data_reg(tmp, p[0] - 1);
+	if (ocp->second == REG_CODE)
+		p[1] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2 + p_sze[0])
+		% MEM_SIZE] - 1);
+	else if (ocp->second == IND_CODE)
+		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 2) %
+		IDX_MOD;
+	while (++i < REG_SIZE)
+		proc->mem[(tmp->pc + p[1] + i) % MEM_SIZE] =
+		tmp->reg[proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1][i];
+	if (cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1) == 0)
+		tmp->carry = 1;
+	else
+		tmp->carry = 0;
+	return (2 + p_sze[0] + p_sze[1]);
 }
