1ffa452131acc5ced3fc27d0bbd0a0a51f508f7a
Amine DERRAGUI <aderragu@e2r3p10.42.fr>
2016-08-11 16:39:30 +0200

    wip st

--- vm/src/cw_ins_st.c
+++ vm/src/cw_ins_st.c
@@ -26,17 +26,24 @@ int			cw_ins_st(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	i = -1;
 	p_sze[0] = 1;
 	p_sze[1] = cw_ins_param_sze(ocp->second, 2);
+
 	p[0] = proc->mem[(tmp->pc + 2) % MEM_SIZE];
-	p[0] = cw_get_data_reg(tmp, p[0] - 1); 
+	p[0] = cw_get_data_reg(tmp, p[0] - 1);
+
+
 	if (ocp->second == REG_CODE)
 		p[1] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2 + p_sze[0])
 		% MEM_SIZE] - 1); 
 	else if (ocp->second == IND_CODE)
-		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 2) %
-		IDX_MOD;
+		p[1] = proc->mem[(tmp->pc + 2 + p_sze[0])] % IDX_MOD;
+
+
 	while (++i < REG_SIZE)
-		proc->mem[(tmp->pc + p[1] + i) % MEM_SIZE] =
+		proc->mem[(p[1] + i) % MEM_SIZE] =
 		tmp->reg[proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1][i];
+
+
+
 	if (cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1) == 0)
 		tmp->carry = 1;
 	else
