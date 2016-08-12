2637a84ac06ae1797ffc72296fb16d060ce3813e
Alain DEMENET <ademenet@student.42.fr>
2016-08-12 16:04:50 +0200

    Ajout de protection contre mauvais num de registres

--- vm/src/cw_ins_and.c
+++ vm/src/cw_ins_and.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:14:06 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/12 11:47:34 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/12 16:04:13 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,28 +40,26 @@ int			cw_ins_and(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 	unsigned int	total;
 
 	total = 0;
-	// on recup les tailles
 	p_sze[0] = cw_ins_param_sze(ocp->first, 4);
 	p_sze[1] = cw_ins_param_sze(ocp->second, 4);
 	p_sze[2] = cw_ins_param_sze(ocp->third, 4);
-	// convertit 1er param
-	if (ocp->first == REG_CODE)
+	if (ocp->first == REG_CODE && proc->mem[(tmp->pc + 2) % MEM_SIZE] > 0 &&
+		proc->mem[(tmp->pc + 2) % MEM_SIZE] <= REG_SIZE)
 		p[0] = cw_get_data_reg(tmp, proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1);
 	else if (ocp->first == DIR_CODE)
 		p[0] = cw_get_data_dir(proc, tmp, tmp->pc + 2, 4);
 	else if (ocp->first == IND_CODE)
 		p[0] = cw_get_data_ind(proc, tmp, tmp->pc + 2);
-	// convertit le 2eme param
-	if (ocp->second == REG_CODE)
+	if (ocp->second == REG_CODE &&
+		proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] > 0 &&
+		proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] <= REG_SIZE)
 		p[1] = cw_get_data_reg(tmp,
 			proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] - 1);
 	else if (ocp->second == DIR_CODE)
 		p[1] = cw_get_data_dir(proc, tmp, tmp->pc + 2 + p_sze[0], 4);
 	else if (ocp->second == IND_CODE)
 		p[1] = cw_get_data_ind(proc, tmp, tmp->pc + 2 + p_sze[0]);
-	// le 3eme param est toujours un registre : p[2] = numero du registre
 	p[2] = proc->mem[(tmp->pc + 2 + p_sze[0] + p_sze[1]) % MEM_SIZE];
-	// total = p1 and p0
 	total = p[1] & p[0];
 	cw_exec_and(tmp, total, p);
 	return (2 + p_sze[0] + p_sze[1] + p_sze[2]);
