84180eb56aa8ed7d507a2770ffeacac4b6e7212b
Amine DERRAGUI <aderragu@e2r7p21.42.fr>
2016-08-17 10:35:22 +0200

    nettoyage commentaire

--- vm/src/cw_ins_st.c
+++ vm/src/cw_ins_st.c
@@ -35,14 +35,14 @@ int			cw_ins_st(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
 	unsigned int	p_sze[2];
 	short int		p[2];
-	int				ind_reg;
-	int				i;
+	unsigned int	ind_reg;
+	unsigned int	i;
 
 	i = -1;
 	p_sze[0] = 1;
 	p_sze[1] = cw_ins_param_sze(ocp->second, 2);
 	ind_reg = proc->mem[(tmp->pc + 2) % MEM_SIZE] - 1;
-	if (ind_reg < 0 || proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] - 1 < 0)
+	if ((int)ind_reg < 0 || (proc->mem[(tmp->pc + 2 + p_sze[0]) % MEM_SIZE] - 1 < 0 && ocp->second == REG_CODE))
 		return (2 + p_sze[0] + p_sze[1]);
 	p[0] = cw_get_data_reg(tmp, ind_reg);
 	if (ocp->second == REG_CODE)
