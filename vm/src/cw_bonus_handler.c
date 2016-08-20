954c3a162ccb70843498bf37a2550fb752806e8b
Amine DERRAGUI <aderragu@e2r6p16.42.fr>
2016-08-20 16:48:39 +0200

    correction seg fault dans la gestion des parametres

--- vm/src/cw_bonus_handler.c
+++ vm/src/cw_bonus_handler.c
@@ -13,15 +13,15 @@
 #include "../inc/corewar.h"
 
 /*	Pour ajouter la gestion d'une option, ajouter la lettre choisie dans la
-	string de ft_cinstr
-	/!\ ne pas ajouter la lettre 'n'
+**	string de ft_cinstr
+**	/!\ ne pas ajouter la lettre 'n'
 */
 void	cw_bon_handler(char **av, int ac, int param)
 {
 	arr_set(g_bon, 0, 256);
 	while (param < ac)
 	{
-		if (av[param][0] == '-' && ft_cinstr(param[av][1], "v"))
+		if (av[param][0] == '-' && ft_cinstr(param[av][1], "vd"))
 			g_bon[av[param][1]] = 1;
 		else
 		{
