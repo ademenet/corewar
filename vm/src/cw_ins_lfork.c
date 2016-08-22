19b0d69497c535737b6d6a965461a77d3b54c1df
Alain DEMENET <ademenet@student.42.fr>
2016-08-22 18:33:31 +0200

    modif de lfork pour coller au nouveau fork

--- vm/src/cw_ins_lfork.c
+++ vm/src/cw_ins_lfork.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/21 16:07:20 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/20 15:43:45 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/08/22 16:55:01 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,16 @@
 
 int			cw_ins_lfork(t_proc *proc, t_champion *tmp, t_ocp *ocp)
 {
-	unsigned int	p;
+	short int	p;
 
-	p = (short)cw_get_data_dir(proc, tmp, (tmp->pc + 1) % MEM_SIZE, 2);
-	cw_lst_push(&proc->champions, cw_lst_new(tmp->header, tmp->num));
-	proc->champions->ins = NULL;
+	p = (short int)cw_get_data_dir(proc, tmp, (tmp->pc + 1) % MEM_SIZE, 2);
+	cw_lst_push(&proc->champions,
+		cw_lst_new(tmp->header, proc->champions->num));
+	cw_ins_fork_duplicate_reg(proc->champions, tmp);
 	proc->champions->pc_origin = tmp->pc_origin;
 	proc->champions->pc = (tmp->pc + (unsigned short)p) % MEM_SIZE;
 	proc->champions->carry = tmp->carry;
-	proc->champions->inst_c = 0;
+	proc->champions->inst_c = g_op[proc->mem[proc->champions->pc] - 1].cycles_nb;
 	proc->champions->lives = 0;
 	proc->champions->id = tmp->id;
 	proc->nb_proc++;
