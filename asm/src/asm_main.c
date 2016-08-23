556d751cc2e261f7ea091f8a8d608ff92f469dde
Geoffrey SEROPIAN <gseropia@e2r6p19.42.fr>
2016-08-23 10:50:08 +0200

    multifiles ok

--- asm/src/asm_main.c
+++ asm/src/asm_main.c
@@ -6,11 +6,18 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 15:50:23 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/02 11:09:40 by gseropia         ###   ########.fr       */
+/*   Updated: 2016/08/23 10:44:09 by gseropia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/asm.h"
+
+void		asm_init_global()
+{
+	g_line = 0;
+	g_pos = 0;
+	g_temp = 0;
+}
 
 int		main(int argc, char **argv)
 {
@@ -18,15 +25,17 @@ int		main(int argc, char **argv)
 	int			index;
 
 	index = 1;
-	g_line = 0;
-	g_pos = 0;
-	g_temp = 0;
 	head = NULL;
-	if (argc == 2)
+	asm_init_global();
+	if (argc > 1)
 	{
-		head = asm_header_init();
-		asm_parsing(argv[index], head);
-		g_pos = 0;
+		while (index < argc)
+		{
+			head = asm_header_init();
+			asm_parsing(argv[index++], head);
+			head = NULL;
+			asm_init_global();
+		}
 	}
 	else
 	{
