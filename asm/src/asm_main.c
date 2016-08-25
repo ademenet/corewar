a104029ed8dcfed01eb0e56e8580de3ac592db88
Visentin <tvisenti@42.student.fr>
2016-08-25 16:24:49 +0200

    Ajout check pour le bonus

--- asm/src/asm_main.c
+++ asm/src/asm_main.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 15:50:23 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/25 15:14:15 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/08/25 16:19:29 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,20 @@ int		main(int argc, char **argv)
 {
 	t_header	*head;
 	int			index;
+	int			check;
 
 	index = 1;
+	check = 0;
 	head = NULL;
 	asm_init_global();
 	if (argc > 1)
 	{
+		if (argv[index][0] == '-' && argv[index][1] == 'b' && ++index)
+			check = 1;
 		while (index < argc)
 		{
 			head = asm_header_init();
-			asm_parsing(argv[index++], head);
+			asm_parsing(argv[index++], head, check);
 			head = NULL;
 			asm_init_global();
 		}
