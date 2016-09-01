489dee52570e3024337b4509af6595059d2470ec
Visentin <tvisenti@42.student.fr>
2016-09-01 17:04:21 +0200

    Fork et lfork ok

--- vm/src/cw_ins_fork.c
+++ vm/src/cw_ins_fork.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/13 15:16:29 by ademenet          #+#    #+#             */
-/*   Updated: 2016/08/31 14:04:59 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/09/01 16:54:36 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 ** hérite des différents états du père.
 */
 
-void		cw_ins_fork_db(t_proc *proc, t_champion *tmp, t_ocp *ocp,
+void		cw_ins_fork_db(t_proc *proc, t_p *tmp, t_ocp *ocp,
 			short int p)
 {
-	ft_printf("P%5d | %s %d (%d)\n", tmp->idp,
+	ft_printf("P%5d | %s %d (%d)\n", tmp->id,
 		"fork", p, (tmp->pc + (p % IDX_MOD)) % MEM_SIZE);
 }
 
-void		cw_ins_fork_duplicate_reg(t_champion *new, t_champion *old)
+void		cw_ins_fork_duplicate_reg(t_p *new, t_p *old)
 {
 	int		i;
 	int		j;
@@ -45,43 +45,21 @@ void		cw_ins_fork_duplicate_reg(t_champion *new, t_champion *old)
 	}
 }
 
-void		cw_ins_fork_init(t_proc *proc)
-{
-	if (proc->mem[proc->champions->pc] > 0x00 &&
-		proc->mem[proc->champions->pc] < 0x11)
-	{
-		proc->champions->inst_c =
-			g_op[proc->mem[proc->champions->pc] - 1].cycles_nb;
-		proc->champions->inst_num =
-			g_op[proc->mem[proc->champions->pc] - 1].opcode;
-		proc->champions->ins = (unsigned char *)1;
-	}
-	else
-	{
-		proc->champions->inst_c = 0;
-		proc->champions->inst_num = 0;
-		proc->champions->ins = NULL;
-	}
-}
-
-int			cw_ins_fork(t_proc *proc, t_champion *tmp, t_ocp *ocp)
+int			cw_ins_fork(t_proc *proc, t_p *tmp, t_ocp *ocp)
 {
 	short int	p;
-	t_champion	*new;
+	t_p			*new;
 
 	p = (short int)cw_get_data_dir(proc, tmp, (tmp->pc + 1) % MEM_SIZE, 2);
-	new = cw_lst_new(tmp->header, proc->champions->num);
-	proc->champions = cw_lst_push(proc, new);
-	cw_ins_fork_duplicate_reg(proc->champions, tmp);
-	proc->champions->pc_origin = tmp->pc_origin;
-	proc->champions->pc = (tmp->pc + (unsigned short)(p % IDX_MOD)) % MEM_SIZE;
-	proc->champions->carry = tmp->carry;
-	cw_ins_fork_init(proc);
-	proc->champions->lives = 0;
-	proc->champions->id = tmp->id;
+	new = cw_lst_new(proc->champions[tmp->id_champion - 1].num);
+	proc->process = cw_lst_push(proc, new);
+	cw_ins_fork_duplicate_reg(new, tmp);
+	new->pc = (tmp->pc + (unsigned short)(p % IDX_MOD)) % MEM_SIZE;
+	new->carry = tmp->carry;
+	cw_get_opcode(proc, new);
+	new->lives = 0;
+	new->id = proc->total_id++;
 	proc->nb_proc++;
-	proc->champions->idp = proc->nb_proc;
-	proc->champions->is_champ = 0;
 	if (g_bon['d'] == 1)
 		cw_ins_fork_db(proc, tmp, ocp, p);
 	return (3);
